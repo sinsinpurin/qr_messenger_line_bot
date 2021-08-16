a354bd4b75167f40bd7e198f511a80a0e434a6ee
sinsinpurin <tusanch6@gmail.com>
2021-08-16 22:15:04 +0900

    test

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import * as line from '@line/bot-sdk';
+//import * as line from '@line/bot-sdk';
 import express from "express";
 const app = express();
 
@@ -8,16 +8,16 @@ const app = express();
 //CHANNEL_SECRET channel secret
 //CHANNEL_TOKEN アクセストークン
 
-const config: line.ClientConfig = {
-    channelAccessToken: process.env.CHANNEL_TOKEN!,
-    channelSecret: process.env.CHANNEL_SECRET!
-};
+// const config: line.ClientConfig = {
+//     channelAccessToken: process.env.CHANNEL_TOKEN!,
+//     channelSecret: process.env.CHANNEL_SECRET!
+// };
 
-const middlewareConfig : line.MiddlewareConfig = {
-    channelSecret : process.env.CHANNEL_SECRET!
-}
+// const middlewareConfig : line.MiddlewareConfig = {
+//     channelSecret : process.env.CHANNEL_SECRET!
+// }
 
-const client = new line.Client(config);
+// const client = new line.Client(config);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -28,20 +28,20 @@ app.get('/', (req, res) => {
     res.send('hello world')
   })
 
-app.post('/callback', line.middleware(middlewareConfig), (req, res) => {
-    Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
-});
+// app.post('/callback', line.middleware(middlewareConfig), (req, res) => {
+//     Promise
+//     .all(req.body.events.map(handleEvent))
+//     .then((result) => res.json(result));
+// });
 
-function handleEvent(event:line.WebhookEvent) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-      return Promise.resolve(null);
-    }
+// function handleEvent(event:line.WebhookEvent) {
+//     if (event.type !== 'message' || event.message.type !== 'text') {
+//       return Promise.resolve(null);
+//     }
   
-    return client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: event.message.text
-    });
-  }
+//     return client.replyMessage(event.replyToken, {
+//       type: 'text',
+//       text: event.message.text
+//     });
+//   }
 
